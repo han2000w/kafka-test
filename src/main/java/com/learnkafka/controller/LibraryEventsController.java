26cf122960452056fead6083a2574cdba99e2f05
han2000w <han2000w@naver.com>
2023-06-20 14:36:04 +0900

    second

---
+++ src/main/java/com/learnkafka/controller/LibraryEventsController.java
@@ -0,0 +1,39 @@
+package com.learnkafka.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.learnkafka.domain.LibraryEvent;
+import com.learnkafka.domain.LibraryEventType;
+import com.learnkafka.producer.LibraryEventProducer;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
+
+@RestController
+@Slf4j
+public class LibraryEventsController {
+
+    private final LibraryEventProducer libraryEventProducer;
+
+    public LibraryEventsController(LibraryEventProducer libraryEventProducer) {
+        this.libraryEventProducer = libraryEventProducer;
+    }
+
+    @PostMapping("/v1/libraryevent")
+    public ResponseEntity<LibraryEvent> postLibraryEvent (@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException, ExecutionException, InterruptedException, TimeoutException {
+
+        log.info("libraryEvent : {}", libraryEvent);
+        // invoke the kafka producer
+        //libraryEventProducer.sendLibraryEvent(libraryEvent);
+        //libraryEventProducer.sendLibraryEvent_approach2(libraryEvent);
+        libraryEventProducer.sendLibraryEvent_approach3(libraryEvent);
+
+        log.info("After Sending libraryEvent : ");
+        return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
+    }
+}
